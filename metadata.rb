9bf19f8992d8ac8d8ca9a9d185caf8cb905126cf
nat <nat.lownes@gmail.com>
2013-01-15 20:16:40 -0500

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ name             'eucalyptus'
 license          "Apache 2.0"
 description      "Installs/Configures eucalyptus-cookbook"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.21"
+version          "0.0.3"
 
 attribute 'euca/front_end',
   :description => %{Your cloud controller's (resolvable) hostname or IP address},
